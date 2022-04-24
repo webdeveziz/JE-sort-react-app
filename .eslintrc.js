dd7db332d03d6f60a38983464496a7618e3b3cbd
Eziz <38165397+webdeveziz@users.noreply.github.com>
2022-04-24 10:51:17 +0300

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ['plugin:react/recommended', 'standard'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 2],
+        semi: ['error', 'never'],
+        'space-before-function-paren': [
+            'error',
+            { anonymous: 'always', named: 'never', asyncArrow: 'always' }
+        ],
+        'multiline-ternary': ['error', 'always-multiline'],
+        'no-constant-condition': ['error', { checkLoops: true }],
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
+        'no-trailing-spaces': ['error', { skipBlankLines: true }],
+        'dot-notation': ['error', { allowKeywords: false }],
+        'spaced-comment': ["error", "always"]
+    }
+}
